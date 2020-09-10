e1e15e2c650397c3ac8d7ec998ed8d40ee96af3a
jhonysmith <evesnin68@gmail.com>
2020-09-10 22:55:33 +0300

    Стилизация меню

--- source/js/index.js
+++ source/js/index.js
@@ -1,6 +1,9 @@
 const withJsHidden = document.querySelectorAll('.with-js-hidden');
 const withoutJs = document.querySelectorAll('.without-js-hidden');
 const menuToggle = document.querySelector('.page-header__toggle');
+const closeMenuToggle = document.querySelector('.page-header__close-toggle');
+const menu = document.querySelector('.main-nav');
+const menuHidden = true;
 
 withJsHidden.forEach((block) => {
   block.classList.add('hidden');
@@ -10,3 +13,13 @@ withoutJs.forEach((block) => {
   block.classList.remove('visually-hidden');
 });
 
+menuToggle.addEventListener('click', () => {
+  menu.classList.add('main-nav--js');
+  menuToggle.classList.add('hidden');
+  closeMenuToggle.classList.remove('hidden');
+  closeMenuToggle.addEventListener('click', () => {
+    menu.classList.remove('main-nav--js');
+    menuToggle.classList.remove('hidden');
+    closeMenuToggle.classList.add('hidden');
+  })
+});
